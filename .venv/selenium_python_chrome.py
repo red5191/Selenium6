f33ae172f809b4858cfa2642248e2978312a3c52
red5191 <the_red51@outlook.com>
2024-12-07 17:21:55 +0300

    123

---
+++ .venv/selenium_python_chrome.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach",True)
+driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+driver.maximize_window()
---
+++ .venv/selenium_python_opera.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach",True)
+driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+driver.maximize_window()
+
+OperaDriver.driver = OperaDriver();
+driver.get("http://selenium2.ru/");
+driver.quit();
